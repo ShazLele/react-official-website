1f949b912fd2837a7a101d2da3e708424ff17454
ShazLeLe <donghuanjie1995@163.com>
2017-06-12 20:30:27 +0800

    3-Components-and-Props [temp]

--- 3-Components-and-Props/index.js
+++ 3-Components-and-Props/index.js
@@ -2,3 +2,12 @@
 /**
  * Created by shaz on 2017/6/12.
  */
+
+function Welcome(props){
+    return <h1>Hello!{props.name}</h1>;
+}
+class Welcome extends React.Component{
+    render(){
+        return <h1>Hello!{this.props.name}</h1>;
+    }
+}
